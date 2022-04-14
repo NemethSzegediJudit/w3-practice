7aecabcd7a8c83465aa4cbfe94f480ed4b90f76d
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-04-14 13:16:40 +0200

    thursday lesson things

---
+++ frontend/data.js
@@ -0,0 +1,8 @@
+let v1 = myFacouriteCountry.tld[0]
+console.log(v1)
+
+let v2 = myFacouriteCountry.name.nativeName.spa.official
+console.log(v2)
+
+let v3 = myFacouriteCountry.idd.suffixes[0]
+console.log(v3)
